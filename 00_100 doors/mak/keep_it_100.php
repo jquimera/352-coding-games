d59dfdc4543b5a1e8b31d47419d7ea855eaf132e
Magbic Aleman <cibgam@gmail.com>
2015-10-05 02:59:17 -0400

    My solution to this game.

---
+++ 00_100 doors/mak/keep_it_100.php
@@ -0,0 +1,110 @@
+<?php
+
+// KOOKY AF 
+// If your computer has php, just run this in the terminal 'php keep_it_100.php'
+// To check if you have php just run 'which php' in your terminal
+
+eval(
+	base64_decode(
+		'JGh1bWFuX2Z1bmN0aW9uYWxpdHkgPSBhcnJheSgNCgknY29tbXVuaWNhdGUnID0+IGFyc
+		mF5KA0KCQknYXJncycgPT4gJyRzdHJpbmcnLA0KCQknY29kZScgPT4gJ2VjaG8gJHN0cmluZz
+		snDQoJKSwNCgknYnVpbGQnID0+IGFycmF5KA0KCQknYXJncycgPT4gJyRpdGVtJywNCgkJJ2N
+		vZGUnID0+ICdlY2hvICJidWlsZGluZyAkaXRlbSI7Jw0KCSkNCik7DQoNCiRzb2x1dGlvbnNf
+		c2V0cyA9IGFycmF5KA0KCSdvbmVfaHVuZHJlZF9kb29ycycgPT4gYXJyYXkoDQoJCSdhcmdzJ
+		yA9PiAnJywNCgkJJ2NvZGUnID0+ICcNCgkJCS8vIEkgaG9wZSB5b3UgZ290IGludGVybmV0IC
+		0uLQ0KCQkJb2Jfc3RhcnQoKTsNCgkJCSRkb2MgPSBuZXcgRE9NRG9jdW1lbnQoKTsNCgkJCUA
+		kZG9jLT5sb2FkSFRNTEZpbGUoImh0dHA6Ly9yb3NldHRhY29kZS5vcmcvd2lraS8xMDBfZG9v
+		cnMjUEhQIik7DQoJCQkkeHBhdGggPSBuZXcgRE9NWHBhdGgoJGRvYyk7DQoJCQkkcGhwX2VsZ
+		W1lbnQgPSAkeHBhdGgtPnF1ZXJ5KCIvLypbQGNsYXNzPVwncGhwIGhpZ2hsaWdodGVkX3NvdX
+		JjZVwnXSIpOw0KDQoJCQlmb3JlYWNoKCRwaHBfZWxlbWVudCBhcyAkZWxlbWVudCkgew0KCQk
+		JCSRjb2RlID0gc3Vic3RyKCRlbGVtZW50LT5ub2RlVmFsdWUsIDUsIC0yKTsNCgkJCQllY2hv
+		IGZpbHRlcl92YXIoJGNvZGUsIEZJTFRFUl9VTlNBRkVfUkFXLCBGSUxURVJfRkxBR19TVFJJU
+		F9ISUdIKTsNCgkJCQkvLyBKdXN0IGdyYWIgdGhlIGZpcnN0IHNvbHV0aW9uDQoJCQkJYnJlYW
+		s7DQoJCQl9DQoJCQkkc29sdXRpb24gPSBvYl9nZXRfY29udGVudHMoKTsNCgkJCW9iX2VuZF9
+		jbGVhbigpOw0KCQkJZXZhbCgkc29sdXRpb24pOycNCgkpDQopOw=='
+	)
+);
+
+// For an instance we get the whole life thing working
+$god = new Life();
+// Cause I'm only going as far as 4 dimensions
+$god->spawn('Space');
+$god->spawn('Time');
+
+// Let the whole life thing simmer a bit
+$time = new Time();
+$time->spawn('Earth');
+
+// Habitable ! Word?! 
+$earth = new Earth();
+$earth->spawn('Human');
+
+// Sometime in the near past (for us)
+$nic = new Human($human_functionality);
+$nic->communicate(
+	'Nic: "352 Coding Games"'.
+	"\n".
+	'Nic: "Solve it you noobs"'.
+	"\n"
+);
+
+$mak = new Human($human_functionality);
+$mak->communicate(
+	'Mak: "Aw shit... he put the link to all the answers -.- "'.
+	"\n".
+	'Mak: "but where do these doors come from?"'.
+	"\n".
+	'Mak, writes a uncessary code'.
+	"\n"
+);
+$mak->communicate(
+	'Going to grab the solution from the site...'.
+	"\n".
+	'I hope you got all of your internets running.'.
+	"\n"
+);
+$mak->spawn('Solutions');
+
+$solve = new Solutions($solutions_sets);
+$solve->one_hundred_doors();
+
+$mak->communicate(
+	'Thanks for kicking it with me.'.
+	"\n"
+);
+
+// Nintendo[64] with it 
+// SSdtIGp1c3QgdHJvbGxpbmcgbm93IC0uLQ==
+
+abstract class Magic {
+
+	private $essence = <<<FORMULA
+Y2xhc3MgJXMgZXh0ZW5kcyBNYWdpYyANCnsNCglwdWJsaWMgZnVuY3Rpb24gX19jb25zdHJ1Y3
+QoJGluc3RydWN0aW9ucyA9IGFycmF5KCkpIA0KICB7DQogICAgaWYgKCAhIGVtcHR5KCRpbnN0
+cnVjdGlvbnMpICYmIGlzX2FycmF5KCRpbnN0cnVjdGlvbnMpKXsNCiAgICAgIGZvcmVhY2goJG
+luc3RydWN0aW9ucyBhcyAkZnVuX3NldCA9PiAkcGFyYW1zKSB7DQogICAgICAgICRhcmdzID0g
+JHBhcmFtc1snYXJncyddOw0KICAgICAgICAkY29kZSA9ICRwYXJhbXNbJ2NvZGUnXTsNCiAgIC
+AgICAgJHRoaXMtPnskZnVuX3NldH0gPSBjcmVhdGVfZnVuY3Rpb24oJGFyZ3MsJGNvZGUpOw0K
+ICAgICAgfQ0KICAgIH0NCgl9DQogIC8vaHR0cDovL3N0YWNrb3ZlcmZsb3cuY29tL3F1ZXN0aW
+9ucy8zMjMxMzY1L2R5bmFtaWNhbGx5LWNyZWF0ZS1pbnN0YW5jZS1tZXRob2QtaW4tcGhwDQog
+IHB1YmxpYyBmdW5jdGlvbiBfX2NhbGwoJG1ldGhvZCwgJGFyZ3MpDQogICB7DQogICAgICAgaW
+YocHJvcGVydHlfZXhpc3RzKCR0aGlzLCAkbWV0aG9kKSkgew0KICAgICAgICAgICBpZihpc19j
+YWxsYWJsZSgkdGhpcy0+JG1ldGhvZCkpIHsNCiAgICAgICAgICAgICAgIHJldHVybiBjYWxsX3
+VzZXJfZnVuY19hcnJheSgkdGhpcy0+JG1ldGhvZCwgJGFyZ3MpOw0KICAgICAgICAgICB9DQog
+ICAgICAgfQ0KICAgfQ0KfQ==
+FORMULA;
+
+	public function spawn ($name) {
+		// A little necessary evil
+		eval(sprintf( base64_decode($this->essence), $name) );
+	}
+
+}
+
+class Life extends Magic {
+
+	/**
+	* Mah-Mah-Mah Magic! 
+	**/
+
+}
