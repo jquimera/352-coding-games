25a4480582f94c9d4197b056578b40814d389440
Nic Rosental <nic@epiclabs.com>
2015-10-05 15:20:29 -0400

    nic's solution

---
+++ 00_100 doors/nicdev/100doors.php
@@ -0,0 +1,24 @@
+<?php 
+
+$limit = 100; // max number of passes through all doors
+$pass = 1; // start on the first door
+$doors = array_fill(1, 100, 'closed'); // set up the XX doors
+
+while($pass <= $limit) {
+  // reset $skip to begin at the $pass index, and skip 
+  // as many doors
+  $skip = $pass; 
+
+  // Pass through the doors
+  while($skip <= $limit) {
+    $doors[$skip] = $doors[$skip] === 'open' ? 'closed' : 'open';
+    $skip += $pass; //next door to check
+  }
+
+  $pass++; // increase pass counter
+}
+
+// output the final state
+foreach($doors as $k => $d) {
+  echo "Door $k is $d <br>";
+}
